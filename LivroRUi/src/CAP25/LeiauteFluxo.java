5415fbbcc8e54c8b8c2b3d7d1d6c200401993fd2
jonathan <jonathangsantos99@gmail.com>
2018-03-22 18:48:13 -0300

    alterou os bts

--- LivroRUi/src/CAP25/LeiauteFluxo.java
+++ LivroRUi/src/CAP25/LeiauteFluxo.java
@@ -1,5 +1,7 @@
 package CAP25;
 
+import java.awt.FlowLayout;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
@@ -15,5 +17,24 @@ public class LeiauteFluxo extends JFrame {
 		setSize(300, 200);
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 
+		FlowLayout fl = new FlowLayout(FlowLayout.LEFT, 10, 10);
+		getContentPane().setLayout(fl);
+
+		bt1 = new JButton("Botão 1");
+		bt2 = new JButton("Botão 2");
+		bt3 = new JButton("Botão 3");
+		bt4 = new JButton("Botão 4");
+		bt5 = new JButton("Botão 5");
+
+		getContentPane().add(bt1);
+		getContentPane().add(bt2);
+		getContentPane().add(bt3);
+		getContentPane().add(bt4);
+		getContentPane().add(bt5);
+
+	}
+
+	public static void main(String[] args) {
+		new LeiauteFluxo().setVisible(true);
 	}
 }
