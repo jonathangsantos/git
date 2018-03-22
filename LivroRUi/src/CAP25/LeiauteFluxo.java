b2af8a4f31782805735ab252f3d06aa0b24216d4
jonathan <jonathangsantos99@gmail.com>
2018-03-22 18:18:52 -0300

    Colocado mais alguns botões

--- LivroRUi/src/CAP25/LeiauteFluxo.java
+++ LivroRUi/src/CAP25/LeiauteFluxo.java
@@ -6,5 +6,8 @@ import javax.swing.JFrame;
 public class LeiauteFluxo extends JFrame {
 	private JButton bt1;
 	private JButton bt2;
+	private JButton bt3;
+	private JButton bt4;
+	private JButton bt5;
 	
 }
