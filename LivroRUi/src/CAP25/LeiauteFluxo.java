f02e2443ebe27f97568096f9073236a8b5ef2481
jonathan <jonathangsantos99@gmail.com>
2018-03-22 18:42:35 -0300

    colocado leiaute

--- LivroRUi/src/CAP25/LeiauteFluxo.java
+++ LivroRUi/src/CAP25/LeiauteFluxo.java
@@ -9,5 +9,11 @@ public class LeiauteFluxo extends JFrame {
 	private JButton bt3;
 	private JButton bt4;
 	private JButton bt5;
-	
+
+	public LeiauteFluxo() {
+		setTitle("Leiaute de fluxo");
+		setSize(300, 200);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
+	}
 }
